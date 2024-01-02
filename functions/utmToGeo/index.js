d1da258dc4cbbac672b5cc3c26e5b287d965beb9
Manuel Pereira <manilex2@gmail.com>
2024-01-02 18:29:46 -0500

    Actualizada funcion de coordenadas

--- functions/utmToGeo/index.js
+++ functions/utmToGeo/index.js
@@ -2,7 +2,7 @@
 const {onRequest} = require("firebase-functions/v2/https");
 const {setGlobalOptions} = require("firebase-functions/v2");
 const admin = require("firebase-admin");
-const {GeoPoint} = require("firebase-admin/firestore");
+const {GeoPoint, getFirestore} = require("firebase-admin/firestore");
 const proj4 = require("proj4");
 
 admin.initializeApp();
@@ -13,15 +13,27 @@ setGlobalOptions({
 });
 
 const utmToGeo = async (req, res) => {
-  admin.auth();
-  proj4.defs("EPSG:32717", "+proj=utm +zone=17 +south +datum=WGS84 +units=m +no_defs");
-  const utm = proj4("EPSG:32717");
-  const xUtm = req.body.utmX;
-  const yUtm = req.body.utmY;
-  const [lon, lat] = utm.inverse([xUtm, yUtm]);
-  const geoPoint = new GeoPoint(lat, lon);
-  console.log(geoPoint);
-  res.send({status: "Success!", message: geoPoint});
+  try {
+    admin.auth();
+    const db = getFirestore();
+    proj4.defs("EPSG:32717", "+proj=utm +zone=17 +south +datum=WGS84 +units=m +no_defs");
+    const utm = proj4("EPSG:32717");
+    const idProy = req.body.idProy;
+    const proyDoc = (await db.collection("proyectos").doc(idProy).get()).data();
+    const xUtm = proyDoc.coordWSG84.utmX;
+    const yUtm = proyDoc.coordWSG84.utmY;
+    const [lon, lat] = utm.inverse([xUtm, yUtm]);
+    const geoPoint = new GeoPoint(lat, lon);
+    console.log(geoPoint);
+    const coordGeo = {
+      coordGeo: geoPoint,
+    };
+    await db.collection("proyectos").doc(idProy).update(coordGeo);
+    res.send({status: "Success!", message: coordGeo});
+  } catch (error) {
+    console.error(error);
+    res.send({status: "Error!", message: error});
+  }
 };
 
 exports.utmToGeo = onRequest(utmToGeo);
